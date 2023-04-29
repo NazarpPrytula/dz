aa87821b9880761e8655ce8ae6425862304e4596
NazarpPrytula <106173960+NazarpPrytula@users.noreply.github.com>
2023-04-30 01:31:18 +0300

    Delete urls.py

--- dz3/hw3/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from hw3.views import PostView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('Post/', include(PostView)),
-]
